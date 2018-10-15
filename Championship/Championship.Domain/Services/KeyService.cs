7f0c9f63ae880cd0b28f9e9170eb8eadfab04fb8
Luis Junqueira | Stone <ljunqueira@stone.com.br>
2018-10-15 16:03:33 -0300

    commit

--- Championship/Championship.Domain/Services/KeyService.cs
+++ Championship/Championship.Domain/Services/KeyService.cs
@@ -1,7 +1,23 @@
 ﻿
+using Championship.Domain.Entities;
+using Championship.Domain.Interfaces;
+using Championship.Domain.Interfaces.Services;
+
 namespace Championship.Domain.Services
 {
-   public class KeyService
+    public class KeyService : ServiceBase<Key>, IKeyService
     {
+        private readonly IkeyRepository _keyRepository;
+
+        public KeyService(IkeyRepository keyrepository)
+            :base(keyrepository)
+        {
+            _keyRepository = keyrepository;
+        }
+
+        public void Generate(Key obj)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
